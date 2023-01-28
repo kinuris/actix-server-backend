3a71bdc747d7e03203756017607561f0ba7a238d
Chris Dominic Chan <chanchrisdominic@gmail.com>
2023-01-28 10:32:15 +0800

    added enums as gql results

--- src/graphql_types.rs
+++ src/graphql_types.rs
@@ -1,6 +1,8 @@
 pub mod input_types {
     use async_graphql::InputObject;
 
+    use crate::models::NewFoodMenuItemVariant;
+
     #[derive(Debug, InputObject)]
     pub struct FoodMenuInput {
         pub name: String,
@@ -15,6 +17,12 @@ pub mod input_types {
         pub stock: i32,
     }
 
+    impl FoodMenuVariantInput {
+        pub fn as_insertable(&self, id: uuid::Uuid) -> NewFoodMenuItemVariant {
+            NewFoodMenuItemVariant { food_menu_id: id, variant_name: &self.variant_name, price: self.price, stock: self.stock }
+        }
+    }
+
     #[derive(Debug, InputObject)]
     pub struct RegisterFoodAndVariants {
         pub food: FoodMenuInput,
@@ -23,7 +31,7 @@ pub mod input_types {
 }
 
 pub mod output_types {
-    use async_graphql::{ComplexObject, SimpleObject};
+    use async_graphql::{ComplexObject, Enum, SimpleObject, Union};
 
     use crate::models::{FoodMenuItem, FoodMenuItemVariant};
 
@@ -45,11 +53,39 @@ pub mod output_types {
         }
     }
 
+    impl From<FoodMenuItem> for FoodMenuOutput {
+        fn from(
+            FoodMenuItem {
+                id,
+                category,
+                img_link,
+                name,
+            }: FoodMenuItem,
+        ) -> Self {
+            Self {
+                id,
+                category,
+                img_link,
+                name,
+            }
+        }
+    }
+
     #[derive(SimpleObject, Debug)]
     pub struct FoodMenuVariantOutput {
         pub variant_name: String,
         pub price: i32,
         pub stock: i32,
+    }
+
+    impl From<FoodMenuItemVariant> for FoodMenuVariantOutput {
+        fn from(value: FoodMenuItemVariant) -> Self {
+            Self {
+                variant_name: value.variant_name,
+                price: value.price,
+                stock: value.stock,
+            }
+        }
     }
 
     #[derive(SimpleObject, Debug)]
@@ -61,9 +97,76 @@ pub mod output_types {
     impl From<(FoodMenuItem, Vec<FoodMenuItemVariant>)> for FoodAndVariantsOutput {
         fn from(value: (FoodMenuItem, Vec<FoodMenuItemVariant>)) -> Self {
             FoodAndVariantsOutput {
-                food: value.0.into_output_type(),
-                variants: value.1.into_iter().map(|x| x.into_output_type()).collect(),
+                food: value.0.into(),
+                variants: value.1.into_iter().map(|x| x.into()).collect(),
             }
         }
+    }
+
+    #[derive(Union)]
+    pub enum RegisterFoodStatus {
+        Status(RegisterFoodStateWrapper),
+        Message(StringWrapper)
+    }
+
+    // SUGGESTION: Put wrappers in their own module
+    #[derive(SimpleObject)]
+    pub struct RegisterFoodStateWrapper {
+        pub state: RegisterFoodState
+    }
+
+    impl From<RegisterFoodState> for RegisterFoodStateWrapper {
+        fn from(value: RegisterFoodState) -> Self {
+            RegisterFoodStateWrapper { state: value }
+        }
+    }
+
+    #[derive(SimpleObject)]
+    pub struct StringWrapper {
+        pub state: String
+    }
+
+    impl From<String> for StringWrapper {
+        fn from(value: String) -> Self {
+            StringWrapper { state: value }
+        }
+    }
+
+    #[derive(Enum, Copy, Clone, PartialEq, Eq, Debug)]
+    pub enum RegisterFoodState {
+        MustBeAdmin
+    }
+
+    #[derive(Enum, Copy, Clone, PartialEq, Eq, Debug)]
+    pub enum SignupStatus {
+        Success,
+        EmailNotAvailable,
+        UsernameNotAvailable,
+        InvalidPasswordCallingTheFBI,
+        SessionStillValid,
+    }
+
+    #[derive(Enum, Copy, Clone, PartialEq, Eq)]
+    pub enum LoginStatus {
+        Success,
+        WrongEmailOrPassword,
+        SessionStillValid,
+    }
+
+    #[derive(Enum, Copy, Clone, PartialEq, Eq)]
+    pub enum DeleteFoodStatus {
+        Success,
+        FoodDoesNotExist,
+        MustBeAdmin,
+    }
+
+    #[derive(Enum, Copy, Clone, PartialEq, Eq)]
+    pub enum RegisterFoodVariantWithId {
+        Success,
+        MustBeAdmin,
+        FoodDoesNotExist,
+        InvalidId,
+        VariantAlreadyExists,
+        UnknownError,
     }
 }
